0bbda052d3c4110292422888175fa563003a3aea
ajxu2 <63217938+ajxu2@users.noreply.github.com>
2022-02-09 22:30:43 -0600

    Add CF 1325A

---
+++ Codeforces/1325A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a;
+        cin >> a;
+        cout << "1 " << a-1 << "\n";
+    }
+    return 0;
+}
