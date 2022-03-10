bdfae29859489c39179f3930ba775e7fb9a9ad78
Anthony Xu <63217938+ajxu2@users.noreply.github.com>
2022-03-10 15:04:37 -0600

    Add CF 1651A

---
+++ Codeforces/1651A.cpp
@@ -0,0 +1,15 @@
+// created: 03-10-2022 Thu 10:57 AM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    cout << (1<<n)-1 << "\n";
+}
+
+int main() {
+    int T; cin >> T;
+    while (T--) solve(); 
+    return 0;
+}
