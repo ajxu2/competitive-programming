b0bc96ee2d662af2dcd143db17db22808012fb3f
Anthony Xu <63217938+ajxu2@users.noreply.github.com>
2022-06-09 17:17:42 -0500

    Add USACO Gold Timeline

---
+++ USACO/Gold/feb20/Timeline.java
@@ -0,0 +1,108 @@
+// created: 06-09-2022 Thu 05:03 PM
+
+import java.util.*;
+import java.io.*;
+
+public class Timeline {
+    public static void main(String[] args) throws IOException {
+        FastIO io = new FastIO("timeline.in", "timeline.out");
+        int n = io.nextInt(), m = io.nextInt(), c = io.nextInt();
+        ArrayList<Pair>[] adj = new ArrayList[n+1], badj = new ArrayList[n+1];
+        int[] in = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            adj[i] = new ArrayList<>(); badj[i] = new ArrayList<>();
+        }
+        int[] dp = new int[n+1];
+        for (int i = 1; i <= n; i++) dp[i] = io.nextInt();
+        for (int i = 0; i < c; i++) {
+            int a = io.nextInt(), b = io.nextInt(), x = io.nextInt();
+            adj[a].add(new Pair(b, x)); badj[b].add(new Pair(a, x)); in[b]++;
+        }
+        ArrayList<Integer> topo = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) if (in[i] == 0) q.add(i);
+        while (!q.isEmpty()) {
+            int process = q.remove();
+            topo.add(process);
+            for (Pair i : adj[process]) {
+                in[i.f]--;
+                if (in[i.f] == 0) q.add(i.f);
+            }
+        }
+        for (int i : topo) {
+            for (Pair j : badj[i]) dp[i] = Math.max(dp[i], dp[j.f] + j.s);
+        }
+        for (int i = 1; i <= n; i++) io.println(dp[i]);
+        io.close();
+    }
+    static class Pair {
+        public int f, s;
+        public Pair(int f, int s) {
+            this.f = f; this.s = s;
+        }
+    }
+    // credits to usaco.guide team for this template
+    static class FastIO extends PrintWriter {
+        private InputStream stream;
+        private byte[] buf = new byte[1<<16];
+        private int curChar, numChars;
+        // standard input
+        public FastIO() { this(System.in,System.out); }
+        public FastIO(InputStream i, OutputStream o) {
+            super(o);
+            stream = i;
+        }
+        // file input
+        public FastIO(String i, String o) throws IOException {
+            super(new FileWriter(o));
+            stream = new FileInputStream(i);
+        }
+        // throws InputMismatchException() if previously detected end of file
+        private int nextByte() {
+            if (numChars == -1) throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars == -1) return -1; // end of file
+            }
+            return buf[curChar++];
+        }
+        // to read in entire lines, replace c <= ' '
+        // with a function that checks whether c is a line break
+        public String next() {
+            int c; do { c = nextByte(); } while (c <= ' ');
+            StringBuilder res = new StringBuilder();
+            do { res.appendCodePoint(c); c = nextByte(); } while (c > ' ');
+            return res.toString();
+        }
+        public int nextInt() {
+            int c; do { c = nextByte(); } while (c <= ' ');
+            int sgn = 1; if (c == '-') { sgn = -1; c = nextByte(); }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res = 10*res+c-'0';
+                c = nextByte();
+            } while (c > ' ');
+            return res * sgn;
+        }
+        public long nextLong() {
+            int c; do { c = nextByte(); } while (c <= ' ');
+            int sgn = 1; if (c == '-') { sgn = -1; c = nextByte(); }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res = 10*res+c-'0';
+                c = nextByte();
+            } while (c > ' ');
+            return res * sgn;
+        }
+        public double nextDouble() { return Double.parseDouble(next()); }
+    }
+}
