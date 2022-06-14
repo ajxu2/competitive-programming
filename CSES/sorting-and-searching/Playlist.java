553b2d27d5bbde8e2d6248ec1bfb0aaae8ac58cd
Anthony Xu <63217938+tony-jp-xu@users.noreply.github.com>
2022-06-14 14:32:06 -0500

    Add CSES Playlist

---
+++ CSES/sorting-and-searching/Playlist.java
@@ -0,0 +1,88 @@
+// created: 06-14-2022 Tue 02:25 PM
+
+import java.util.*;
+import java.io.*;
+
+public class Playlist {
+    public static void main(String[] args) throws IOException {
+        FastIO io = new FastIO();
+        int n = io.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = io.nextInt();
+        int r = 0, ans = 0;
+        Set<Integer> s = new HashSet<>();
+        for (int l = 0; l < n; l++) {
+            while (r < n && !s.contains(a[r])) {
+                s.add(a[r]); r++;
+            }
+            ans = Math.max(ans, r-l);
+            s.remove(a[l]);
+        }
+        io.println(ans);
+        io.close();
+    }
+    // credits to usaco.guide team for this template
+    static class FastIO extends PrintWriter {
+        private InputStream stream;
+        private byte[] buf = new byte[1<<16];
+        private int curChar, numChars;
+        // standard input
+        public FastIO() { this(System.in,System.out); }
+        public FastIO(InputStream i, OutputStream o) {
+            super(o);
+            stream = i;
+        }
+        // file input
+        public FastIO(String i, String o) throws IOException {
+            super(new FileWriter(o));
+            stream = new FileInputStream(i);
+        }
+        // throws InputMismatchException() if previously detected end of file
+        private int nextByte() {
+            if (numChars == -1) throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars == -1) return -1; // end of file
+            }
+            return buf[curChar++];
+        }
+        // to read in entire lines, replace c <= ' '
+        // with a function that checks whether c is a line break
+        public String next() {
+            int c; do { c = nextByte(); } while (c <= ' ');
+            StringBuilder res = new StringBuilder();
+            do { res.appendCodePoint(c); c = nextByte(); } while (c > ' ');
+            return res.toString();
+        }
+        public int nextInt() {
+            int c; do { c = nextByte(); } while (c <= ' ');
+            int sgn = 1; if (c == '-') { sgn = -1; c = nextByte(); }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res = 10*res+c-'0';
+                c = nextByte();
+            } while (c > ' ');
+            return res * sgn;
+        }
+        public long nextLong() {
+            int c; do { c = nextByte(); } while (c <= ' ');
+            int sgn = 1; if (c == '-') { sgn = -1; c = nextByte(); }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9')
+                    throw new InputMismatchException();
+                res = 10*res+c-'0';
+                c = nextByte();
+            } while (c > ' ');
+            return res * sgn;
+        }
+        public double nextDouble() { return Double.parseDouble(next()); }
+    }
+}
