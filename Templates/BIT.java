c7f7fa0cef2327e13e9a8b2ce6f7602926c4c386
Anthony Xu <63217938+tony-jp-xu@users.noreply.github.com>
2022-06-16 14:27:12 -0500

    Add CSES Range Update Queries

--- Templates/BIT.java
+++ Templates/BIT.java
@@ -6,13 +6,17 @@ static class BIT {
         this.n = n;
         tree = new long[n+1];
     }
+    // increase value at position i by v (1-indexed)
+    public void add(int i, long v) {
+        while (i <= n) {
+            tree[i] += v;
+            i += i & -i;
+        }
+    }
     // update value at position i to v (1-indexed)
     public void upd(int i, long v) {
         long d = v - qry(i, i);
-        while (i <= n) {
-            tree[i] += d;
-            i += i & -i;
-        }
+        add(i, d);
     }
     // query the sum from [1, a] (1-indexed)
     public long qry(int a) {
