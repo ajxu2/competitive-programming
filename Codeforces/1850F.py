febe8868e2216af8418bed494110dbf9867ef63e
Anthony Xu <63217938+ajxu2@users.noreply.github.com>
2023-07-24 19:00:47 -0500

    Make CF 1850F not FST

--- Codeforces/1850F.py
+++ Codeforces/1850F.py
@@ -1,13 +1,16 @@
+import sys
+input = sys.stdin.readline
+print = lambda x: sys.stdout.write(str(x)+"\n")
+
 for i in range(int(input())):
     n = int(input())
     a = [*map(int, input().split())]
     b = [0]*(n+1)
-    m = {}
+    m = [0]*(n+1)
     for j in a:
-        if j not in m:
-            m[j] = 0
-        m[j] += 1
-    for j  in m:
+        if j <= n:
+            m[j] += 1
+    for j in range(1, n+1):
         for k in range(j, n+1, j):
             b[k] += m[j]
     print(max(b[1:]))
