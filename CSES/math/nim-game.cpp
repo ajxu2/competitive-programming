852c72ddb5a3e665ad71b37e841e9e2bc6d5237d
Anthony Xu <63217938+ajxu2@users.noreply.github.com>
2023-10-20 01:18:08 -0500

    Add CSES Nim Games
    nim too ez

---
+++ CSES/math/nim-game.cpp
@@ -0,0 +1,21 @@
+// created: 10-20-2023 Fri 01:09 AM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+template<class T> using V = vector<T>;
+template<class T> int sz(const T& a) { return (int)size(a); }
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        V<int> a(n);
+        for (int& i : a) cin >> i;
+        if (accumulate(begin(a), end(a), 0, bit_xor<int>()) == 0) cout << "second\n";
+        else cout << "first\n";
+    }
+    return 0;
+}
