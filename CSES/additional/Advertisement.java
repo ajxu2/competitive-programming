7e01971dda55eb9b53be91a07b7781f117fa2f2f
Anthony Xu <63217938+ajxu2@users.noreply.github.com>
2024-04-16 15:07:12 -0500

    Add CSES Advertisement
    road to 200...

---
+++ CSES/additional/Advertisement.java
@@ -0,0 +1,94 @@
+// created: 04-16-2024 Tue 02:41 PM
+
+import java.util.*;
+import java.io.*;
+
+public class Advertisement {
+    static FastIO io = new FastIO();
+    public static void main(String[] args) throws IOException {
+        int n = io.nextInt();
+        int[] a = new int[n], l = new int[n], r = new int[n];
+        for (int i = 0; i < n; i++) a[i] = io.nextInt();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && a[stack.peek()] >= a[i]) stack.pop();
+            l[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        stack = new ArrayDeque<>();
+        for (int i = n-1; i >= 0; i--) {
+            while (!stack.isEmpty() && a[stack.peek()] >= a[i]) stack.pop();
+            r[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        long ans = 0;
+        for (int i = 0; i < n; i++) ans = Math.max(ans, (long)a[i] * (r[i] - l[i] - 1));
+        io.println(ans);
+        io.close();
+    }
+}
+
+// credits to usaco.guide team for this template
+class FastIO extends PrintWriter {
+    private InputStream stream;
+    private byte[] buf = new byte[1<<16];
+    private int curChar, numChars;
+    // standard input
+    public FastIO() { this(System.in,System.out); }
+    public FastIO(InputStream i, OutputStream o) {
+        super(o);
+        stream = i;
+    }
+    // file input
+    public FastIO(String i, String o) throws IOException {
+        super(new FileWriter(o));
+        stream = new FileInputStream(i);
+    }
+    // throws InputMismatchException() if previously detected end of file
+    private int nextByte() {
+        if (numChars == -1) throw new InputMismatchException();
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = stream.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (numChars == -1) return -1; // end of file
+        }
+        return buf[curChar++];
+    }
+    // to read in entire lines, replace c <= ' '
+    // with a function that checks whether c is a line break
+    public String next() {
+        int c; do { c = nextByte(); } while (c <= ' ');
+        StringBuilder res = new StringBuilder();
+        do { res.appendCodePoint(c); c = nextByte(); } while (c > ' ');
+        return res.toString();
+    }
+    public int nextInt() {
+        int c; do { c = nextByte(); } while (c <= ' ');
+        int sgn = 1; if (c == '-') { sgn = -1; c = nextByte(); }
+        int res = 0;
+        do {
+            if (c < '0' || c > '9')
+                throw new InputMismatchException();
+            res = 10*res+c-'0';
+            c = nextByte();
+        } while (c > ' ');
+        return res * sgn;
+    }
+    public long nextLong() {
+        int c; do { c = nextByte(); } while (c <= ' ');
+        int sgn = 1; if (c == '-') { sgn = -1; c = nextByte(); }
+        long res = 0;
+        do {
+            if (c < '0' || c > '9')
+                throw new InputMismatchException();
+            res = 10*res+c-'0';
+            c = nextByte();
+        } while (c > ' ');
+        return res * sgn;
+    }
+    public double nextDouble() { return Double.parseDouble(next()); }
+}
