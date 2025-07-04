47147df2370fcd2d1e61a27692a540df57f7fe06
Anthony Xu <63217938+ajxu2@users.noreply.github.com>
2025-07-04 00:13:14 -0400

    Add some bitwise stuff
    FIRST UPDATE TO README IN LIKE 4 YEARS HYPE

---
+++ CSES/bitwise/maximum-xor-subset.cpp
@@ -0,0 +1,36 @@
+// created: 07-03-2025 Thu 11:35 PM
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+template<class T> using V = vector<T>;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n; cin >> n;
+    V<int> a(n);
+    for (int& i : a)
+        cin >> i;
+    // gaussian eliminate ts
+    int pivoted = 0;
+    for (int i = 30; i >= 0; i--) {
+        bool found = false;
+        for (int j = pivoted; j < n; j++) {
+            if ((a[j] >> i) & 1) {
+                swap(a[pivoted], a[j]);
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            continue;
+        for (int j = 0; j < n; j++) {
+            if (j != pivoted && ((a[j] >> i) & 1))
+                a[j] ^= a[pivoted];
+        }
+        pivoted++;
+    }
+    cout << accumulate(begin(a), end(a), 0, bit_xor<int>()) << '\n';
+    return 0;
+}
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This repository contains my solutions to various competitive programming problems.
 
-I don't really have anything else to put here so I guess you can have my [Codeforces profile](https://codeforces.com/profile/ajxu2) and my [CSES profile](https://cses.fi/user/42444).
+I don't really have anything else to put here so I guess you can have my [Codeforces profile](https://codeforces.com/profile/ajxu2) and my [CSES profile](https://cses.fi/problemset/user/42444).
