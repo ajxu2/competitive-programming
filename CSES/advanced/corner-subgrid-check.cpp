44bf5d375efda80ee40463602842394be343f142
Anthony Xu <63217938+ajxu2@users.noreply.github.com>
2025-10-15 15:51:43 -0500

    Fix CSES Corner Subgrid Check

--- CSES/advanced/corner-subgrid-check.cpp
+++ CSES/advanced/corner-subgrid-check.cpp
@@ -1,11 +1,13 @@
 // created: 05-29-2025 Thu 04:53 PM
-
+ 
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 using ll = long long;
 template<class T> using V = vector<T>;
-
+ 
+const int MAXN = 3000;
+ 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     int n, k; cin >> n >> k;
@@ -15,23 +17,24 @@ int main() {
         for (int j = 0; j < n; j++)
             a[i][j] = s[j] - 'A';
     }
-    V<V<V<int>>> pos(k, V<V<int>>(n)); // positions of letters per row
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            pos[a[i][j]][i].push_back(j);
+    bitset<MAXN * MAXN> has_pair;
     for (int i = 0; i < k; i++) {
-        V<V<bool>> has_pair(n, V<bool>(n, false));
+        has_pair.reset();
         bool found = false;
-        for (int j = 0; j < n; j++) {
-            for (int q1 : pos[i][j]) {
-                for (int q2 : pos[i][j]) {
-                    if (q1 == q2)
-                        continue;
-                    if (has_pair[q1][q2]) {
+        for (int row = 0; row < n; row++) {
+            V<int> pos;
+            for (int col = 0; col < n; col++)
+                if (a[row][col] == i)
+                    pos.push_back(col);
+            for (int idx1 = 0; idx1 < ssize(pos); idx1++) {
+                for (int idx2 = idx1 + 1; idx2 < ssize(pos); idx2++) {
+                    int q1 = pos[idx1], q2 = pos[idx2];
+                    int idx = q1 * n + q2;
+                    if (has_pair[idx]) {
                         found = true;
                         goto done;
                     }
-                    has_pair[q1][q2] = true;
+                    has_pair[idx] = true;
                 }
             }
         }
