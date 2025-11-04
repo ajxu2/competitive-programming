0da08089976c684475ad506e38c3cad52e486317
Anthony Xu <63217938+ajxu2@users.noreply.github.com>
2025-11-04 18:12:02 -0500

    Add C++23 compilation to run tool

--- Tools/run.py
+++ Tools/run.py
@@ -8,7 +8,7 @@ def compile_file(file, **kwargs):
     p = Path(file)
     ext = p.suffix
     if ext == '.cpp':
-        return run(['g++', '-std=c++20', '-O2', '-DLOCAL', '-Wall', '-Wextra', '-fsanitize=undefined', '-fsanitize=address', '-g', '-o', str(p.with_suffix('.out')), file], **kwargs)
+        return run(['g++', '-std=c++23', '-O2', '-DLOCAL', '-Wall', '-Wextra', '-fsanitize=undefined', '-fsanitize=address', '-g', '-o', str(p.with_suffix('.out')), file], **kwargs)
     elif ext == '.java':
         return run(['javac', file], **kwargs)
     elif ext == '.py':
