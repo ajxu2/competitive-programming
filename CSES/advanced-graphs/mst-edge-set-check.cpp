88116d494e03be87083446ce37e7280a41917d9d
Anthony Xu <63217938+ajxu2@users.noreply.github.com>
2025-11-11 16:05:43 -0500

    Add cleaner (but slower) solution to MST Edge Set Check

--- CSES/advanced-graphs/mst-edge-set-check.cpp
+++ CSES/advanced-graphs/mst-edge-set-check.cpp
@@ -41,66 +41,40 @@ struct DSURollback {
     }
 };
 
-struct Edge {
-    int u, v, w, i;
-    bool operator<(const Edge& o) {
-        if (w == o.w)
-            return i < o.i;
-        return w < o.w;
-    }
-};
+using Edge = array<int, 2>;
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
     int n, m, q; cin >> n >> m >> q;
-    DSURollback dsu(n);
-    V<Edge> edges(m);
-    for (Edge& e : edges) {
-        cin >> e.u >> e.v >> e.w; e.u--, e.v--;
-        e.i = -1;
+    map<int, V<Edge>> edges;
+    V<array<int, 3>> edges_by_idx(m);;
+    for (int i = 0; i < m; i++) {
+        int u, v, w; cin >> u >> v >> w; u--, v--;
+        edges[w].push_back({u, v});
+        edges_by_idx[i] = {u, v, w};
     }
-    V<Edge> queries;
+    map<int, map<int, V<Edge>>> queries;
     for (int i = 0; i < q; i++) {
         int size_set; cin >> size_set;
         while (size_set--) {
             int e_idx; cin >> e_idx; e_idx--;
-            Edge e = edges[e_idx];
-            queries.push_back({e.u, e.v, e.w, i});
+            auto [u, v, w] = edges_by_idx[e_idx];
+            queries[w][i].push_back({u, v});
         }
     }
-    sort(begin(edges), end(edges));
-    sort(begin(queries), end(queries));
+    DSURollback dsu(n);
     V<bool> ans(q, true);
-    int edges_l = 0, queries_l = 0;
-    while (edges_l < m) {
-        int cur_w = edges[edges_l].w;
-        int edges_r = edges_l;
-        while (edges_r < m && edges[edges_r].w == cur_w)
-            edges_r++;
-        int queries_r = queries_l;
-        while (queries_r < ssize(queries) && queries[queries_r].w == cur_w)
-            queries_r++;
-        int cur_query_l = queries_l;
-        while (cur_query_l < queries_r) {
-            int cur_i = queries[cur_query_l].i;
-            int cur_query_r = cur_query_l;
-            while (cur_query_r < queries_r && queries[cur_query_r].i == cur_i)
-                cur_query_r++;
-            for (int i = cur_query_l; i < cur_query_r; i++) {
-                auto [u, v, _, q_idx] = queries[i];
+    for (auto [w, edges_w] : edges) {
+        for (auto [i, queries_wi] : queries[w]) {
+            for (auto [u, v] : queries_wi) {
                 if (!dsu.unite(u, v))
-                    ans[q_idx] = false;
+                    ans[i] = false;
             }
-            for (int i = cur_query_l; i < cur_query_r; i++)
+            for (auto _ : queries_wi)
                 dsu.rollback();
-            cur_query_l = cur_query_r;
         }
-        for (int i = edges_l; i < edges_r; i++) {
-            auto [u, v, _, __] = edges[i];
+        for (auto [u, v] : edges_w)
             dsu.unite(u, v);
-        }
-        edges_l = edges_r;
-        queries_l = queries_r;
     }
     for (bool i : ans)
         cout << (i ? "YES" : "NO") << '\n';
