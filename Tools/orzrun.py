27ede1f13c1b4e9d12db635844752447371e2348
Anthony Xu <63217938+ajxu2@users.noreply.github.com>
2026-02-16 17:30:51 -0500

    Add experimental AtCoder support for orzrun
    doesn't work in contest yet :(

--- Tools/orzrun.py
+++ Tools/orzrun.py
@@ -14,29 +14,39 @@ FAIL = '\033[91m'
 BOLD = '\033[1m'
 END = '\033[0m'
 
+SPOOF_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:147.0) Gecko/20100101 Firefox/147.0' # trol
+
 def owo(text, color):
     return f'{color}{text}{END}'
 
-def cf_check_equality(text1, text2):
+def cp_check_equality(text1, text2):
     ws_annihilator = re.compile(r'\s+')
     return ws_annihilator.sub(' ', text1) == ws_annihilator.sub(' ', text2)
 
 def fetch_test_cases(problem):
-    m = re.fullmatch(r'(\d+)([a-z]\d?)', problem, re.I)
-    if not m:
-        raise ValueError(f'Invalid Codeforces problem ID: {problem}')
-    req = requests.get(f'https://codeforces.com/contest/{m.group(1)}/problem/{m.group(2)}', headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:147.0) Gecko/20100101 Firefox/147.0'}) # trol
-    b = BeautifulSoup(req.text, 'html.parser')
-
-    # get input output pairs
-    raw_inputs = b.find_all('div', attrs={'class': 'input'})
-    raw_outputs = b.find_all('div', attrs={'class': 'output'})
-    res = [tuple(j.pre.get_text('\n', strip=True) for j in i) for i in zip(raw_inputs, raw_outputs)]
-    
+    if m := re.fullmatch(r'(\d+)([a-z]\d?)', problem, re.I):
+        # Codeforces
+        req = requests.get(f'https://codeforces.com/contest/{m[1]}/problem/{m[2]}', headers={'User-Agent': SPOOF_AGENT})
+        b = BeautifulSoup(req.text, 'html.parser')
+        raw_inputs = b.find_all('div', attrs={'class': 'input'})
+        raw_outputs = b.find_all('div', attrs={'class': 'output'})
+        res = [tuple(j.pre.get_text('\n', strip=True) for j in i) for i in zip(raw_inputs, raw_outputs)]
+    elif m := re.fullmatch(r'((abc|arc|agc)\d+)([a-z])', problem, re.I):
+        # Atcoder
+        req = requests.get(f'https://atcoder.jp/contests/{m[1]}/tasks/{m[1]}_{m[3]}', headers={'User-Agent': SPOOF_AGENT})
+        b = BeautifulSoup(req.text, 'html.parser')
+        inputs = []
+        outputs = []
+        for tag in b.find_all('section'):
+            if tag.h3.text.startswith('Sample Input'):
+                inputs.append(tag.pre.text.strip())
+            elif tag.h3.text.startswith('Sample Output'):
+                outputs.append(tag.pre.text.strip())
+        res = list(zip(inputs, outputs))
     return res
 
 def main():
-    parser = ArgumentParser(description='Code runner that automatically fetches Codeforces samples')
+    parser = ArgumentParser(description='Code runner that automatically fetches CF/AC samples')
     parser.add_argument('file', help='the file to orzrun')
     args = parser.parse_args()
 
@@ -64,8 +74,8 @@ def main():
         print(f'sample input:\n{i}\n\nsample output:\n{o}\n\nyour output:\n{user_output}\n')
         if user_stderr:
             print(f'your program wrote this on standard error:\n{user_stderr}\n')
-        summary += f'Test #{cnt+1}: '
-        if cf_check_equality(user_output, o):
+        summary += f'Test #{cnt + 1}: '
+        if cp_check_equality(user_output, o):
             if process.returncode == 0:
                 print(owo('OK\n', GREEN + BOLD))
                 summary += owo('OK\n', GREEN + BOLD)
